e6e86a4b9466bf212b20516750185d294fb887c3
akashvharale <akash.v.harale@gmail.com>
2025-08-05 16:51:14 +0530

    added a new prop - formHeading

--- components/EnrollDialog.tsx
+++ components/EnrollDialog.tsx
@@ -9,9 +9,10 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import BookADemoForm from "@/components/homePage/BookADemoForm";
-import { ReactNode } from "react";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 interface EnrollDialogProps {
+  formHeading?: string;
   buttonText: string;
   className?: string;
   size?: "default" | "sm" | "lg" | "icon";
@@ -25,6 +26,7 @@ interface EnrollDialogProps {
 }
 
 export function EnrollDialog({
+  formHeading,
   buttonText,
   className,
   size = "lg",
@@ -33,19 +35,14 @@ export function EnrollDialog({
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button
-          size={size}
-          variant={variant}
-          className={className}
-        >
+        <Button size={size} variant={variant} className={className}>
           {buttonText}
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-md bg-gradient-to-br from-slate-50 to-blue-50">
-        <DialogHeader>
-          
-        </DialogHeader>
-        <BookADemoForm btnName={buttonText} />
+      <DialogContent className=" bg-gradient-to-br from-slate-50 to-blue-50">
+        <DialogDescription></DialogDescription>
+        <DialogTitle className="hidden">Book a demo</DialogTitle>
+        <BookADemoForm formHeading={formHeading} btnName={buttonText} />
       </DialogContent>
     </Dialog>
   );
